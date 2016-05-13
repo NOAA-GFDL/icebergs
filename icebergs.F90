23390e2bc1cc853d6cbbd890cbf61104e3111bb1
Alon Stern <sternalon@gmail.com>
2016-05-13 12:11:13 -0400

    Added a warning for icebergs being born on land

--- icebergs.F90
+++ icebergs.F90
@@ -1753,6 +1753,10 @@ integer :: stderrunit
             write(stderrunit,*) 'diamonds, calve_icebergs: something went very wrong!',i,j,xi,yj
             call error_mesg('diamonds, calve_icebergs', 'berg xi,yj is not correct!', FATAL)
           endif
+          if (grd%msk(i,j)<0.5) then
+            write(stderrunit,*) 'diamonds, adjust: WARNING!!! Iceberg born in land cell',i,j,newberg%lon,newberg%lat
+            if (debug) call error_mesg('diamonds,calve_icebergs', 'Iceberg born in Land Cell!', FATAL)
+          endif
           newberg%ine=i
           newberg%jne=j
           newberg%xi=xi
